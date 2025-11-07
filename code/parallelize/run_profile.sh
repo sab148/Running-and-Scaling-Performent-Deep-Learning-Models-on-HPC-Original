80ba4c46c7242bcfd6dff44be79d9080a5e264ba
Javad Kasravi <j.kasravi@fz-juelich.de>
2025-11-07 11:05:06 +0100

    add seperated file for profiling

---
+++ code/parallelize/run_profile.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+source /p/scratch/atmlaml/HPC-Supporters-Course/sc_venv_template_HPC_supporter_course/venv/bin/activate
+
+SCRIPT_NAME=$1
+shift
+
+nsys profile \
+    --duration=30 \
+    --delay=100 \
+    --gpu-metrics-device=all \
+    --nic-metrics=true \
+    --stop-on-exit=false \
+    --trace=nvtx,cuda,osrt \
+    --python-sampling=true \
+    --python-sampling-frequency=1 \
+    --cuda-memory-usage=true \
+    --force-overwrite=true \
+    --python-functions-trace=config/profiling.json \
+    --output=nsys_logs/nsys_logs_rank_${RANK} \
+    python -u "$SCRIPT_NAME" "$@"
